c1991bbe71effe5a38bbec94c07842135897d7ad
Cauli Tomaz <caulitomaz@gmail.com>
2015-04-08 16:18:13 -0300

    Replacing TABS with SPACES

--- TwineJson.js
+++ TwineJson.js
@@ -23,292 +23,365 @@
 * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-window.onload = function() {
-	if (typeof(window.TwineJson) == "undefined") {
+window.onload = function () {
+  if (typeof(window.TwineJson) == "undefined") {
+    var idCount = 0;
 
-		var ExportOptions = {
-			// If true, puts every element as a child or grandchild of the Starting Point
-			// WARNING: Hierarchical JSON trees shouldn't be cyclic. This will cause an infinite loop.
-			"isHierarchical":true,
-			// Exports using {{features}}{{/features}} syntax (Should default to false)
-			// * adds the property "star" to the element
-			// + adds the property "delta" to the element
-			// - adds the property "optional" to the element
-			"exportFeatures":true, 
-		};
+    var ExportOptions = {
+      // If true, puts every element as a child or grandchild of the Starting Point
+      // WARNING: Hierarchical JSON trees shouldn't be cyclic. This will cause an infinite loop.
+      "isHierarchical":true,
+      // Exports what is inside of {{order}}{{/order}} to ".order" property on JSON
+      "exportOrder":true,
+      // Exports using {{features}}{{/features}} syntax (Should default to false)
+      // * adds the property "star" to the element
+      // + adds the property "delta" to the element
+      // - adds the property "optional" to the element
+      "exportFeatures":true,
+      // Export Unique ID for node
+      "exportID": true
+    };
 
-		window.TwineJson = {
-			isCyclic: function(obj) {
-			  var seenObjects = [];
+    window.TwineJson = {
+      isCyclic: function (obj) {
+        var seenObjects = [];
 
-			  function detect (obj) {
-			    if (obj && typeof obj === 'object') {
-			      if (seenObjects.indexOf(obj) !== -1) {
-			        return true;
-			      }
-			      seenObjects.push(obj);
-			      for (var key in obj) {
-			        if (obj.hasOwnProperty(key) && detect(obj[key])) {
-			          console.error(obj, ' Found cycle at ' + key);
-			          return true;
-			        }
-			      }
-			    }
-			    return false;
-			  }
+        function detect (obj) {
+          if (obj && typeof obj === 'object')
+          {
+            if (seenObjects.indexOf(obj) !== -1)
+            {
+              return true;
+            }
 
-			  return detect(obj);
-			},
+            seenObjects.push(obj);
 
+            for (var key in obj)
+            {
+              if (obj.hasOwnProperty(key) && detect(obj[key])) {
+                console.error(obj, 'Found cycle at ' + key);
+                return true;
+              }
+            }
+          }
+          
+          return false;
+        }
 
-			convert: function() {
-				var output = window.document.getElementById("output");
-
-				var jsonString = this.export();
-					
-				var originalJsonPlain = JSON.parse(jsonString);
-				var jsonPlain = originalJsonPlain;
-
-
-				/* 
-				* For each of the elements of jsonPlain
-				* Check if it has one or more children
-				* For each child
-				* Loop through all elements of jsonPlain searching for the name of the child
-				* Add to the parent.children array
-				*/ 
-				if(ExportOptions.isHierarchical)
-				{			
-					var hierarchyJSON = [];
-
-					for(var i = 0; i < jsonPlain.length; i ++)
-					{
-						jsonPlain[i].children = [];
-
-						if(jsonPlain[i].childrenNames != "")
-						{	
-							var eachChildren = jsonPlain[i].childrenNames.split(',');
-
-							for(var k = 0; k < eachChildren.length; k++ )
-							{
-								for(var j = 0; j < jsonPlain.length; j++ )
-								{
-									if(eachChildren[k] == "[["+jsonPlain[j].name+"]]")
-									{
-										jsonPlain[i].children.push(jsonPlain[j]);
-									}
-								}
-							}
-							
-							hierarchyJSON.push(jsonPlain[i]);
-						}
-					}
-
-					if(this.isCyclic(hierarchyJSON[0]))
-					{
-						output.innerHTML = "<h2>Error: Cyclic reference found</h2><br>Cyclic references aren't allowed in hierarchical JSON structures.<br>Please set ExportOptions.isHierarchical to false to use cyclic references.";
-					}
-					else
-					{
-						// FIXME Is this always the case, the first index?
-						console.dir(hierarchyJSON[0]);
-						output.innerHTML = JSON.stringify(hierarchyJSON[0]);
-					}
-					
-				}
-				else
-				{
-					output.innerHTML = JSON.stringify(jsonPlain);
-				}
+        return detect(obj);
+      },
 
 
-			
-			},
+      convert: function() {
+        var output = window.document.getElementById("output");
 
-			
-			export: function() {
-				var buffer = [];
-
-				buffer.push("[\r\n"); // Opens JSON
-
-				var storyData = window.document.getElementsByTagName("tw-storydata");
-				if (storyData)
-				{
-					buffer.push(this.buildPassage("StoryTitle","",storyData[0].getAttribute("name")));
-				}
-
-				var userScript = window.document.getElementById("twine-user-script");
-				if (userScript)
-				{
-					buffer.push(this.buildPassage("UserScript","script",userScript.innerHTML));
-				}
-
-				var userStylesheet = window.document.getElementById("twine-user-stylesheet");
-				if (userStylesheet)
-				{
-					buffer.push(this.buildPassage("UserStylesheet","stylesheet",userStylesheet.innerHTML));
-				}
-
-				var passages = window.document.getElementsByTagName("tw-passagedata");
-				for (var i = 0; i < passages.length; ++i)
-				{
-					buffer.push(this.buildPassageFromElement(passages[i], i, passages.length));
-				}
-
-				buffer.push("]\r\n"); // Closes JSON
-
-				return buffer.join('');
-			},
-
-			
-			buildPassageFromElement: function(passage, index, howManyPassages) {
-				var last = false;
-				
-				if(index+1 == howManyPassages)
-				{
-					last = true;
-				}
-				
-				var name = passage.getAttribute("name");
-				if (!name)
-				{
-					name = "Untitled Passage";
-				}
-
-				var tags = passage.getAttribute("tags");
-				var content = passage.textContent;
-				
-				return this.buildPassage(name, tags, content, last);
-			},
-	
-	
-			buildPassage: function(title, tags, content, last) {
-				var result = [];
-				
-				result.push("{\r\n");
-
-				result.push("\t\"name\" : ");
-				result.push("\"",title,"\"");
-
-				if (tags) 
-				{
-					result.push(",\r\n");
-					result.push("\t\"tags\" : ");
-					result.push("\"[",tags,"]\"");
-				}
-
-				var scrubbedContent = this.scrub(content, " ");
-
-				/* Push the content */
-				result.push(",\r\n");
-				result.push("\t\"content\" : ");
-				result.push("\"", scrubbedContent,"\"");
-				
-				result.push(",\r\n");
-				result.push("\t\"childrenNames\" : ");
-				result.push("\"", this.findChildren(scrubbedContent),"\"");
-				
-				if(ExportOptions.exportFeatures)
-				{
-					result.push(",\r\n");
-					result.push("\t\"features\" :");
-					result.push("", this.findFeatures(content),"\r\n");
-				}
-
-				
-				if (!last) 
-				{
-					result.push("},\r\n");
-				}
-				else 
-				{
-					result.push("}\r\n");
-				}
-
-				return result.join('');
-			},
-
-			scrub: function(content, separator) {
-				if(content)
-				{
-					content = content.replace(/\"/gm, "\'");
-
-					// Removes all line breaks
-					content = content.replace(/(\r\n|\n|\r)/gm,separator);
-				}
-
-				return content;
-			},
-
-			// Finds children passages by searching for the [[Passage Name]] syntax
-			findChildren: function(content) {
-				var ptrn = /\[\[(.+?)\]\]/gm;
-				var match;
-				var results = [];
-
-				while( ( match = ptrn.exec(content) ) != null )
-				{
-					results.push(match[0]);
-				}
-
-				return results;
-			},
+        var jsonString = this.export();
+          
+        var originalJsonPlain = JSON.parse(jsonString);
+        var jsonPlain = originalJsonPlain;
 
 
-			// Finds features inside the passage by searching for the {{features}}...{{/features}} syntax
-			findFeatures: function(content) {
-				var ptrn = /\{\{features\}\}((\s|\S)+?)\{\{\/features\}\}/gm;
-				var match;
-				var results = [];
-				var featuresArray = [];
+        /* 
+        *  For each of the elements of jsonPlain
+        *  Check if it has one or more children
+        *  For each child
+        *  Loop through all elements of jsonPlain searching for the name of the child
+        *  Add to the parent.children array
+        */ 
+        if(ExportOptions.isHierarchical)
+        {      
+          var hierarchyJSON = [];
 
-				while( ( match = ptrn.exec(content) ) != null )
-				{
-					var features = match[1].split(/(\r\n|\n|\r|\,)/gm);
+          for(var i = 0; i < jsonPlain.length; i ++)
+          {
+            jsonPlain[i].children = [];
 
-					// Cleans up the features array by removing empty/return entries
-					// TIP: <= 1 because carriage returns counts as one character
-					for(var i = 0; i < features.length; i++)
-					{
-						if(features[i].length <= 1)
-						{
-							features.splice(i,1);
-							i=-1;
-						}
-					}
+            if(jsonPlain[i].childrenNames != "")
+            {
+              var eachChildren = jsonPlain[i].childrenNames.split(',');
 
-					for(var i = 0; i < features.length; i++)
-					{
-						var featureJSON = {};
+              for(var k = 0; k < eachChildren.length; k++ )
+              {
+                for(var j = 0; j < jsonPlain.length; j++ )
+                {
+                  if(eachChildren[k] == "[["+jsonPlain[j].name+"]]")
+                  {
+                    jsonPlain[i].children.push(jsonPlain[j]);
+                  }
+                }
+              }
+              
+              hierarchyJSON.push(jsonPlain[i]);
+            }
+          }
 
-						if(features[i][0] == "*")
-						{
-							featureJSON.star = true;
-							features[i] = features[i].substring(1);
-						}
+          if(this.isCyclic(hierarchyJSON[0]))
+          {
+            output.innerHTML = "<h2>Error: Cyclic reference found</h2><br>Cyclic references aren't allowed in hierarchical JSON structures.<br>Please set ExportOptions.isHierarchical to false to use cyclic references.";
+          }
+          else
+          {
+            // FIXME Is this always the case, the first index?
+            console.dir(hierarchyJSON[0]);
+            output.innerHTML = JSON.stringify(hierarchyJSON[0]);
+          }
+          
+        }
+        else
+        {
+          output.innerHTML = JSON.stringify(jsonPlain);
+        }
 
-						if(features[i][0] == "+")
-						{
-							featureJSON.delta = true;
-							features[i] = features[i].substring(1);
-						}
 
-						if(features[i][0] == "-")
-						{
-							featureJSON.optional = true;
-							features[i] = features[i].substring(1);
-						}
+      
+      },
 
-						featureJSON.name = features[i];
-						featuresArray.push(featureJSON);
-					}
-				}
+      
+      export: function() {
+        var buffer = [];
 
-				return JSON.stringify(featuresArray);
-			}
+        buffer.push("[\r\n"); // Opens JSON
 
-		}			
-	}
-	
-	window.TwineJson.convert();
+        var storyData = window.document.getElementsByTagName("tw-storydata");
+        if (storyData)
+        {
+          buffer.push(this.buildPassage("StoryTitle","",storyData[0].getAttribute("name")));
+        }
+
+        var userScript = window.document.getElementById("twine-user-script");
+        if (userScript)
+        {
+          buffer.push(this.buildPassage("UserScript","script",userScript.innerHTML));
+        }
+
+        var userStylesheet = window.document.getElementById("twine-user-stylesheet");
+        if (userStylesheet)
+        {
+          buffer.push(this.buildPassage("UserStylesheet","stylesheet",userStylesheet.innerHTML));
+        }
+
+        var passages = window.document.getElementsByTagName("tw-passagedata");
+        for (var i = 0; i < passages.length; ++i)
+        {
+          buffer.push(this.buildPassageFromElement(passages[i], i, passages.length));
+        }
+
+        buffer.push("]\r\n"); // Closes JSON
+
+        return buffer.join('');
+      },
+
+      
+      buildPassageFromElement: function(passage, index, howManyPassages) {
+        var last = false;
+        
+        if(index+1 == howManyPassages)
+        {
+          last = true;
+        }
+        
+        var name = passage.getAttribute("name");
+        if (!name)
+        {
+          name = "Untitled Passage";
+        }
+
+        var tags = passage.getAttribute("tags");
+        var content = passage.textContent;
+        
+        return this.buildPassage(name, tags, content, last);
+      },
+  
+  
+      buildPassage: function(title, tags, content, last) {
+        var result = [];
+        
+        result.push("{\r\n");
+
+        if(ExportOptions.exportID)
+        {
+          result.push("\t\"id\" : ");
+          result.push("\"",idCount++,"\"");
+        }
+
+        result.push("\t\"name\" : ");
+        result.push("\"",title,"\"");
+
+        if (tags) 
+        {
+          result.push(",\r\n");
+          result.push("\t\"tags\" : ");
+          result.push("\"[",tags,"]\"");
+        }
+
+        var scrubbedContent = this.scrub(content, " ");
+
+        /* Push the content */
+        result.push(",\r\n");
+        result.push("\t\"content\" : ");
+        result.push("\"", scrubbedContent,"\"");
+        
+        result.push(",\r\n");
+        result.push("\t\"childrenNames\" : ");
+        result.push("\"", this.findChildren(scrubbedContent),"\"");
+        
+        if(ExportOptions.exportFeatures)
+        {
+          result.push(",\r\n");
+          result.push("\t\"features\" :");
+          result.push("", this.findFeatures(content),"\r\n");
+        }
+
+        if(ExportOptions.exportOrder)
+        {
+          result.push(",\r\n");
+          result.push("\t\"order\" :");
+          result.push("", this.findOrder(content),"\r\n");
+        }
+
+        
+        if (!last) 
+        {
+          result.push("},\r\n");
+        }
+        else 
+        {
+          result.push("}\r\n");
+        }
+
+        return result.join('');
+      },
+
+      scrub: function(content, separator) {
+        if(content)
+        {
+          content = content.replace(/\"/gm, "\'");
+
+          // Removes all line breaks
+          content = content.replace(/(\r\n|\n|\r)/gm,separator);
+        }
+
+        return content;
+      },
+
+      // Finds children passages by searching for the [[Passage Name]] syntax
+      findChildren: function(content) {
+        var ptrn = /\[\[(.+?)\]\]/gm;
+        var match;
+        var results = [];
+
+        while( ( match = ptrn.exec(content) ) != null )
+        {
+          results.push(match[0]);
+        }
+
+        return results;
+      },
+
+      // Finds order inside the passage by searching for the {{order}}...{{/order}} syntax
+      findOrder: function(content) {
+        var ptrn = /\{\{order\}\}((\s|\S)+?)\{\{\/order\}\}/gm;
+        var match;
+        var results = [];
+        var orderArray = [];
+
+        while( ( match = ptrn.exec(content) ) != null )
+        {
+          var order = match[1].split(/(\r\n|\n|\r)/gm);
+
+          // Cleans up the order array by removing empty/return entries
+          // TIP: <= 1 because carriage returns counts as one character
+          for(var i = 0; i < order.length; i++)
+          {
+            if(order[i].length <= 1)
+            {
+              order.splice(i,1);
+              i=-1;
+            }
+          }
+
+          for(var i = 0; i < order.length; i++)
+          {
+            featureJSON.order = order[i];
+            orderArray.push(featureJSON);
+          }
+        }
+
+        return JSON.stringify(orderArray);
+      }
+
+      // Finds features inside the passage by searching for the {{features}}...{{/features}} syntax
+      findFeatures: function(content) {
+        var ptrn = /\{\{features\}\}((\s|\S)+?)\{\{\/features\}\}/gm;
+        var match;
+        var results = [];
+        var featuresArray = [];
+
+        while( ( match = ptrn.exec(content) ) != null )
+        {
+          var features = match[1].split(/(\r\n|\n|\r)/gm);
+
+          // Cleans up the features array by removing empty/return entries
+          // TIP: <= 1 because carriage returns counts as one character
+          for(var i = 0; i < features.length; i++)
+          {
+            if(features[i].length <= 1)
+            {
+              features.splice(i,1);
+              i=-1;
+            }
+          }
+
+          for(var i = 0; i < features.length; i++)
+          {
+            var regExp = /\(([^)]+)\)/;
+            var matches = regExp.exec(features[i]);
+
+            var featureJSON = {};
+
+            if(matches != null)
+            {
+              if(matches[1])
+              {
+                features[i] = features[i].replace(/\(([^)]+)\)/gi, "");
+                featureJSON.subtitle = matches[1];
+              }  
+            }
+            
+            if(features[i][0] == ":")
+            {
+              featureJSON.separator = true;
+              features[i] = features[i].substring(1);
+            }
+    
+            if(features[i][0] == "*")
+            {
+              featureJSON.star = true;
+              features[i] = features[i].substring(1);
+            }
+
+            if(features[i][0] == "+")
+            {
+              featureJSON.delta = true;
+              features[i] = features[i].substring(1);
+            }
+
+            if(features[i][0] == "-")
+            {
+              featureJSON.optional = true;
+              features[i] = features[i].substring(1);
+            }
+
+            featureJSON.name = features[i];
+            featuresArray.push(featureJSON);
+          }
+        }
+
+        return JSON.stringify(featuresArray);
+      }
+
+    }      
+  }
+  
+  window.TwineJson.convert();
 }
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,61 @@ var fileincluder = require('gulp-file-includer')
 var jsEscape = require('gulp-js-escape');
 var runSequence = require('run-sequence');
 var clean = require('gulp-clean');
+var jslint = require('gulp-jslint');
+ 
+// build the main source into the min file 
+gulp.task('jslint', function () {
+    return gulp.src(['TwineJson.js'])
+ 
+        // pass your directives 
+        // as an object 
+        .pipe(jslint({
+            // these directives can 
+            // be found in the official 
+            // JSLint documentation. 
+            node: true,
+            evil: true,
+            nomen: true,
+ 
+            // you can also set global 
+            // declarations for all source 
+            // files like so: 
+            global: [],
+            predef: [],
+            // both ways will achieve the 
+            // same result; predef will be 
+            // given priority because it is 
+            // promoted by JSLint 
+ 
+            // pass in your prefered 
+            // reporter like so: 
+            reporter: 'default',
+            // ^ there's no need to tell gulp-jslint 
+            // to use the default reporter. If there is 
+            // no reporter specified, gulp-jslint will use 
+            // its own. 
+ 
+            // specifiy custom jslint edition 
+            // by default, the latest edition will 
+            // be used 
+            edition: '2014-07-08',
+ 
+            // specify whether or not 
+            // to show 'PASS' messages 
+            // for built-in reporter 
+            errorsOnly: false
+        }))
+ 
+        // error handling: 
+        // to handle on error, simply 
+        // bind yourself to the error event 
+        // of the stream, and use the only 
+        // argument as the error object 
+        // (error instanceof Error) 
+        .on('error', function (error) {
+            console.error(String(error));
+        });
+});
 
 gulp.task('fileincluder-html', function() {
   return gulp.src(['storyFormat.html'])
@@ -62,5 +117,5 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', function() {
-  runSequence('uglify', 'fileincluder-html','minify-html','escape-js','fileincluder-format','clean-dist','watch');
+  runSequence( 'jslint','uglify','fileincluder-html','minify-html','escape-js','fileincluder-format','clean-dist','watch');
 });
